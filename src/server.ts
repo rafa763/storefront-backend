2fb4e0f9718d37058a09d5c97af25ff94cbff084
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-08-26 17:38:49 +0200

    configured eb

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import routes from './routes/index';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
